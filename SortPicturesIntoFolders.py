d02cfc64ce4eb98b6c710fc8956013ca9fe3c0ce
Shawwal Shahid <Shawwal2.Shahid@live.uwe.ac.uk>
2025-02-26 23:21:19 +0000

    Folders are now created.

---
+++ SortPicturesIntoFolders.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import shutil
+from pathlib import Path
+
+originalFolderPath = sys.argv[1]
+thisFolderPath = sys.argv[2]
+outputFolderPath = sys.argv[3]
+
+materials = str("materials")
+startChecking = False
+
+for originalPath, originalFolders, originalFiles, in os.walk(originalFolderPath):
+    for originalFileName in originalFiles:
+        splitPath = Path(str(originalPath)).parts
+        startChecking = False
+        fullFolderPath = None 
+        for folders in splitPath:
+            if str(folders) == materials:
+                startChecking = True
+                newOutputFolderPath = os.path.join(str(outputFolderPath), materials)
+                if not os.path.exists(newOutputFolderPath):
+                        os.makedirs(newOutputFolderPath)
+            if startChecking == True:
+                fullFolderPath = str(folders)
+                fullPath = os.path.join(str(newOutputFolderPath), str(fullFolderPath))
+                if not os.path.exists(fullPath):
+                    os.makedirs(fullPath)
+
+
+
+for path, folders, files, in os.walk(thisFolderPath):
+    for fileName in files:
+        for originalPath, originalFolders, originalFiles, in os.walk(originalFolderPath):
+            for originalFileName in originalFiles:
+                if str(Path(fileName).stem) in str(Path(originalFileName).stem):
+                     fullPath = os.path.join(str(thisFolderPath), str(path), str(fileName))
+                     shutil.copy2(fullPath, outputFolderPath)
+
+
+
