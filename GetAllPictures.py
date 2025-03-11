cc3915dce2368fb9c4b6fd0404bc16bcd6fdeba4
Shawwal Shahid <Shawwal2.Shahid@live.uwe.ac.uk>
2025-03-11 00:16:56 +0000

    Not getting all the pictures anymore

--- GetAllPictures.py
+++ GetAllPictures.py
@@ -5,11 +5,18 @@ import shutil
 originalFolderPath = sys.argv[1]
 toCopyFolder = sys.argv[2]
 
+allStrings = ["cable", "composite", "console", "customcubemaps", "debug", "effects", "environment maps", "gamepadui", "hlmv", "matsys_regressiontest", "particle", "perftest", "scripted", "shadertest", "sun", "tools", "vgui", "voice", "sprites"]
+
 for path, folders, files, in os.walk(originalFolderPath):
     for fileName in files:
         if "_normal" in str(fileName):
             print("saffsa")
         else:
-            fullPath = os.path.join(str(originalFolderPath), str(path), str(fileName))
-            print(fullPath)
-            shutil.copy2(fullPath, toCopyFolder)
+            do = False
+            for string in allStrings:
+                if string in str(path):
+                    do = True
+                    break
+            if do == False:
+                fullPath = os.path.join(str(originalFolderPath), str(path), str(fileName))
+                shutil.copy2(fullPath, toCopyFolder)
